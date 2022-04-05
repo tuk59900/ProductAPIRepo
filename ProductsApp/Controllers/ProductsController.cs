caaa42658d52eddb9cc791c4924efe14ff4e824f
tuk59900 <tuk59900@temple.edu>
2022-04-05 14:23:10 -0400

    Changed values in ProductsController.cs Lines 14, 15, and 16 as instructed.

--- ProductsApp/Controllers/ProductsController.cs
+++ ProductsApp/Controllers/ProductsController.cs
@@ -11,9 +11,9 @@ namespace ProductsApp.Controllers
     {
         Product[] products = new Product[]
         {
-            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
-            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
-            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+            new Product { Id = 1, Name = "Carrot Soup", Category = "Food", Price = 4 },
+            new Product { Id = 2, Name = "Football", Category = "Sport", Price = 5M },
+            new Product { Id = 3, Name = "Drill", Category = "Tools", Price = 16.99M }
         };
 
         public ProductsController()
